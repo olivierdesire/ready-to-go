112aa7b63fa856e9026dc44c125f9cdd7321cd70
Olivier Désiré <olivierdesire@MacBook-Pro-de-Olivier.local>
2022-12-22 22:53:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import logo from "./assets/img/navette.png";
 
 function App() {
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
+
+  let buttonGo = "No way !";
+  if (switch1 && switch2 && switch3) {
+    buttonGo = "Go !";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo} alt="navette" />
+        <p> Ready To Go</p>
       </header>
+      <main>
+        <div className="choice">
+          <div>
+            <div>
+              <button
+                onClick={() => {
+                  setSwitch1(true);
+                }}
+                style={{
+                  backgroundColor: switch1 ? "#5C48D3" : "white",
+                  color: switch1 ? "white" : "#5C48D3",
+                }}
+              >
+                ON
+              </button>
+              <button
+                onClick={() => {
+                  setSwitch1(false);
+                }}
+                style={{
+                  backgroundColor: switch1 ? "white" : "#5C48D3",
+                  color: switch1 ? "#5C48D3" : "white",
+                }}
+              >
+                OFF
+              </button>
+            </div>
+            <div>
+              <button
+                onClick={() => {
+                  setSwitch2(true);
+                }}
+                style={{
+                  backgroundColor: switch2 ? "#5C48D3" : "white",
+                  color: switch2 ? "white" : "#5C48D3",
+                }}
+              >
+                ON
+              </button>
+              <button
+                onClick={() => {
+                  setSwitch2(false);
+                }}
+                style={{
+                  backgroundColor: switch2 ? "white" : "#5C48D3",
+                  color: switch2 ? "#5C48D3" : "white",
+                }}
+              >
+                OFF
+              </button>
+            </div>
+            <div>
+              <button
+                onClick={() => {
+                  setSwitch3(true);
+                }}
+                style={{
+                  backgroundColor: switch3 ? "#5C48D3" : "white",
+                  color: switch3 ? "white" : "#5C48D3",
+                }}
+              >
+                ON
+              </button>
+              <button
+                onClick={() => {
+                  setSwitch3(false);
+                }}
+                style={{
+                  backgroundColor: switch3 ? "white" : "#5C48D3",
+                  color: switch3 ? "#5C48D3" : "white",
+                }}
+              >
+                OFF
+              </button>
+            </div>
+          </div>
+          <div
+            className="valid"
+            style={{
+              backgroundColor: buttonGo === "Go !" ? "#46AC4A" : "#D34849",
+            }}
+          >
+            {buttonGo}
+          </div>
+        </div>
+      </main>
+      <footer>
+        Made with <span>React</span> at <span>Le Reacteur</span> by{" "}
+        <span>Olivier</span>
+      </footer>
     </div>
   );
 }
